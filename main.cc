564e3fc2c5ce52afe35d0895ccd522a6e6dfb3bf
Alex Yang <web@alexyang.dev>
2022-03-30 00:40:20 -0700

    Create SDL2 window

--- main.cc
+++ main.cc
@@ -1,14 +1,19 @@
 #include <iostream>
 
-#include "SDL.h"
+#include <SDL2/SDL.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #endif
 
-void mainLoop(double dt) {
-  std::cout << dt << std::endl;
+bool mainLoop(double dt) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+  if (event.type == SDL_QUIT) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -19,37 +24,50 @@ EM_BOOL emLoop(double time, void *userData) {
   // calls to this function will not reset this value.
   static double prevTime = 0;
 
-  double dt = time - prevTime;
+  const double dt = time - prevTime;
   prevTime = time;
 
-  mainLoop(dt);
+  const bool shouldQuit = mainLoop(dt);
 
-  return EM_TRUE;
+  return shouldQuit ? EM_TRUE : EM_FALSE;
 }
 #endif
 
 int main(int argc, char **argv) {
 
+  SDL_Window* window;
+  SDL_Renderer* renderer;
+
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+    std::cout << "Unable to initialize SDL: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer)) {
+    std::cout << "Couldn't create window and renderer: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
 #ifdef __EMSCRIPTEN__
-
   emscripten_request_animation_frame_loop(emLoop, nullptr);
-
 #else
-
   double dt;
   double prevTicks = 0;
+  bool shouldQuit = false;
 
-  while (true) {
-
+  while (!shouldQuit) {
     double ticks = SDL_GetTicks();
     dt = ticks - prevTicks;
     prevTicks = ticks;
-
-    mainLoop(dt);
-
+    shouldQuit = mainLoop(dt);
     SDL_Delay(17);
   }
-
 #endif
 
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
+  return 0;
 }
